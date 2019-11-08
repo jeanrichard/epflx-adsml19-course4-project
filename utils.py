a23a1199d49b1ee5daaa4ed2c1bc7baddb45bca9
Richard Etienne, INI-DNA-NB <Etienne.Richard@swisscom.com>
2019-11-08 18:35:54 +0100

    Part 04: Code complete

--- utils.py
+++ utils.py
@@ -4,11 +4,14 @@ Utilities for Course 4 - Project.
 """
 
 # Standard library.
+import functools
+import operator
 import os
 import typing as T
 
 # 3rd party.
 import numpy as np
+import pandas as pd
 
 
 def load(path: os.PathLike) -> T.Mapping[str, np.ndarray]:
@@ -30,3 +33,20 @@ def info(data: T.Mapping[str, np.ndarray]) -> str:
         f'{key}: shape={val.shape}, dtype={val.dtype}'
         for key, val in data.items()
     ])
+
+
+def filter_like(df: pd.DataFrame, idx: T.Any, columns: T.Sequence[str]) -> pd.DataFrame:
+    """\
+    DOCME
+    """
+    # We need to be careful: None values are never equal to other values in Pandas.
+    def eq(series: pd.Series, value: T.Optional[T.Any]) -> pd.Series:
+        return series.isna() if value is None else series == value
+
+    conditions = [
+        eq(df[column], df.loc[idx, column]) for column in columns
+    ]
+
+    is_like = functools.reduce(operator.and_, conditions)
+
+    return df[is_like]
